b293b4a2fa3fa501fef26cfba4137db93d3eaf05
HamzaYslmn <hamzayesilmen7@gmail.com>
2025-02-28 02:44:42 +0300

    Add YouTube to MP3 conversion feature with web interface

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse, HTMLResponse
+import xPyTube
+import jinja2
+import urllib.parse
 
 app = FastAPI(
     root_path="/APP",
@@ -7,13 +12,28 @@ app = FastAPI(
     docs_url="/docs"  # None
 )
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = jinja2.Environment(loader=jinja2.FileSystemLoader("static"))
+
 @app.get("/online")
 async def online():
     return {"status": "online"}
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def read_root():
-    return {"root": "root"}
+    template = templates.get_template("index.html")
+    return HTMLResponse(template.render())
+
+@app.post("/download")
+async def download_file(url: str = Form(...)):
+    result = xPyTube.convert_to_mp3(url)
+    if result:
+        mp3_path, name = result
+        headers = {
+            "Content-Disposition": f"attachment; filename*=UTF-8''{urllib.parse.quote(name)}"
+        }
+        return FileResponse(mp3_path, media_type='audio/mpeg', headers=headers)
+    return {"error": "Error occurred"}
 
 if __name__ == "__main__":
     import uvicorn
---
+++ app/xPyTube.py
@@ -0,0 +1,34 @@
+from pytubefix import YouTube
+from moviepy import AudioFileClip
+import os
+
+def convert_to_mp3(video_url):
+    try:
+        # Download the YouTube video
+        yt = YouTube(video_url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        audio_stream = audio_stream.download()
+        
+        # Prepare the MP3 file path by replacing the extension
+        mp3_path = os.path.splitext(audio_stream)[0] + '.mp3'
+        
+        # Extract the audio using AudioFileClip
+        with AudioFileClip(audio_stream) as clip:
+            clip.write_audiofile(mp3_path)
+        
+        # Optionally, remove the original audio file
+        os.remove(audio_stream)
+                
+        # Return the MP3 path and its filename as dynamic name
+        return mp3_path, os.path.basename(mp3_path)
+    
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    video_url = input("Youtube video URL'si girin: ")
+    result = convert_to_mp3(video_url)
+    if result:
+        mp3_path, name = result
+        print(f"MP3 dosyası oluşturuldu: {mp3_path}")
+        print(f"MP3 dosyası adı: {name}")
