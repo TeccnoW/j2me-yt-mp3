627f6a8bdcf857321aa09f81223aa38897333934
HamzaYslmn <hamzayesilmen7@gmail.com>
2025-02-28 15:38:21 +0300

    Fix start.sh to exit if Cloudflare WARP is OFF and add wait command to keep the script running

--- start.sh
+++ start.sh
@@ -10,8 +10,9 @@ if curl -s https://www.cloudflare.com/cdn-cgi/trace | grep -q 'warp=on'; then
   echo "Warp is ON."
 else
   echo "Warp is OFF. Exiting."
-  exit 1
-fi
 
 # Start the FastAPI application
 exec uvicorn main:app --host 0.0.0.0 --port 8001
+
+# wait forever
+wait
