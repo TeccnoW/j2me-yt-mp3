5841fdce36f1cb210e238e903554d572fe122f14
HamzaYslmn <hamzayesilmen7@gmail.com>
2025-02-28 16:20:01 +0300

    Comment out Cloudflare WARP connection commands in start.sh for debugging purposes

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Connecting Cloudflare Warp VPN..."
-warp-cli connect
-echo "Warp VPN connected."
-
+# echo "Connecting Cloudflare Warp VPN..."
+# warp-cli connect
+# echo "Warp VPN connected."
+# 
 # Check Cloudflare connection status
 # if curl -s https://www.cloudflare.com/cdn-cgi/trace | grep -q 'warp=on'; then
 #   echo "Warp is ON."
